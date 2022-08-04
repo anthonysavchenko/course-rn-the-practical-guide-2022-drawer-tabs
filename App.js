b8f380408c4d4a6152965c9722dc9e4bd8441592
Anthony Savchenko <anthony.savchenko@gmail.com>
2022-08-04 18:51:34 +1000

    Initiating drawer

--- App.js
+++ App.js
@@ -1,5 +1,17 @@
-import { Text } from 'react-native';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { UserScreen } from "./screens/UserScreen";
+import { WelcomeScreen } from "./screens/WelcomeScreen";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return <Text>Todo...</Text>;
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="User">
+        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+        <Drawer.Screen name="User" component={UserScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
